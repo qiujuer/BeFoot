ad98ddefdc08b77e03661819e820ea16eb57202a
qiujuer <qiujuer@live.cn>
2016-05-20 17:58:36 +0800

    Add ConstraintLayout sample

---
+++ blog/sample/ConstraintLayout/app/src/main/java/net/qiujuer/sample/constraintlayout/MainActivity.java
@@ -0,0 +1,13 @@
+package net.qiujuer.sample.constraintlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done);
+    }
+}
